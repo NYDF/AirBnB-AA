32a2fdde806bdfbef7773ac659478ab64570371d
dongfang <yusu.us@gmail.com>
2022-08-26 13:44:11 -0400

    added firstname and lastname to seeders

--- backend/db/seeders/20220825204658-demo-user.js
+++ backend/db/seeders/20220825204658-demo-user.js
@@ -7,16 +7,22 @@ module.exports = {
       {
         email: 'demo@user.io',
         username: 'Demo-lition',
+        firstName: 'Demoo',
+        lastName: 'litionn',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user1@user.io',
         username: 'FakeUser1',
+        firstName: 'Demoo1',
+        lastName: 'litionn1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'user2@user.io',
         username: 'FakeUser2',
+        firstName: 'Demoo2',
+        lastName: 'litionn2',
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], {});
