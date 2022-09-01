2e36bb674b8bce484d49c8adc5e32735f358f9be
dongfang <yusu.us@gmail.com>
2022-09-01 10:44:49 -0400

    added more seeders

--- backend/db/seeders/20220829215938-demo-user.js
+++ backend/db/seeders/20220829215938-demo-user.js
@@ -12,17 +12,38 @@ module.exports = {
         hashedPassword: bcrypt.hashSync('password')
       },
       {
-        email: 'user1@user.io',
+        email: 'user2@user.io',
         username: 'FakeUser2',
         firstName: 'adam2',
         lastName: 'laure2',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
-        email: 'user2@user.io',
+        email: 'user3@user.io',
         username: 'FakeUser3',
         firstName: 'adam3',
         lastName: 'laure3',
+        hashedPassword: bcrypt.hashSync('password3')
+      },
+      {
+        email: 'demo4@user.io',
+        username: 'Demo-lition4',
+        firstName: 'adam4',
+        lastName: 'laure4',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user5@user.io',
+        username: 'FakeUser5',
+        firstName: 'adam5',
+        lastName: 'laure5',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'user6@user.io',
+        username: 'FakeUser6',
+        firstName: 'adam6',
+        lastName: 'laure6',
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], {});
@@ -31,7 +52,7 @@ module.exports = {
   down: async (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete('Users', {
-      username: { [Op.in]: ['Demo-lition1', 'FakeUser2', 'FakeUser3'] }
+      username: { [Op.in]: ['Demo-lition1', 'FakeUser2', 'FakeUser3','Demo-lition4', 'FakeUser5', 'FakeUser6'] }
     }, {});
   }
 };
