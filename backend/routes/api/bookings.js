61afbfc0e2e920736ba738615094931781aa29fa
dongfang <yusu.us@gmail.com>
2022-09-01 18:41:43 -0400

    still try

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -24,7 +24,7 @@ router.get(
                 raw: true, nest: true
             })
             console.log('!!!!!', prevImage)
-            booking.Spot.previewImage = prevImage.url
+            booking.Spot.previewImage = prevImage
 
         }
 
