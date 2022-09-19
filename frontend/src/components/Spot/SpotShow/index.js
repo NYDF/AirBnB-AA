c039e968c9589f1ccc927d477dbeb32601db0499
dongfang <yusu.us@gmail.com>
2022-09-19 09:24:54 -0400

    edited spotshow

--- frontend/src/components/Spot/SpotShow/index.js
+++ frontend/src/components/Spot/SpotShow/index.js
@@ -1,14 +1,40 @@
-// import { useParams } from 'react-router-dom';
 
-// function SpotShow({ spot }) {
+import React from 'react';
+import ListingMap from './../listing_map';
+import ReviewIndexContainer from './../../review/review_index_container'
+import { fetchReviews } from './../../../actions/review_actions';
+import { withRouter } from 'react-router-dom';
+import CreateReservationFormContainer from './../../reservation/create_reservation_form_container';
+import ReservationCalendar from './../../reservation/reservation_calendar';
+import * as miscUtil from './../../../util/misc_util'
 
-//   return (
-//     <div className="spot-show">
-//       {/* <h2>{spot.name}</h2>
-//       <div>Color: {spot.city}</div> */}
-//       fdsafdsafdsafdsa
-//     </div>
-//   );
-// }
+const SpotShow = ({ spots }) => {
+    const { id } = useParams();
+    const singleSpot = spots[id];
 
-// export default SpotShow;
+    const { city, state, name, price, avgRating, previewImage } = spot.spot;
+
+    return (
+        <div className='spot-show-container'>
+
+            <div className='title-container'>
+                <span> &#9733; </span>
+                <span> {avgRating || "New"} </span>
+                <span> · </span>
+                `${city}, ${state}`
+            </div>
+
+            <div className='image-container'>
+                <img className="main-image" src={previewImage} />
+            </div>
+
+            <div className='spot-show-description-container'>
+                <div className='spot-show-name'>${name}</div>
+                <div className='spot-show-price'>${price}</div>
+                <div className='spot-show-description'>${name}</div>
+            </div>
+        </div>
+    );
+};
+
+export default SingleArticle;
