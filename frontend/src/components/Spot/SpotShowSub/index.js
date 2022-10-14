cd2c4633a32eb47c956e4c515c7292cf25426d2f
dongfang <yusu.us@gmail.com>
2022-10-13 20:02:56 -0400

    some css

--- frontend/src/components/Spot/SpotShowSub/index.js
+++ frontend/src/components/Spot/SpotShowSub/index.js
@@ -56,6 +56,14 @@ const SpotShowSub = () => {
                 </div>
             </div>
 
+            <hr></hr>
+
+            <div className="air-cover-img-container">
+                <img className="air-cover-img"
+                src="https://a0.muscache.com/im/pictures/54e427bb-9cb7-4a81-94cf-78f19156faad.jpg" alt="AIR COVER" />
+            </div>
+            <div>Every booking includes free protection from Host cancellations, listing inaccuracies, and other issues like trouble checking in.</div>
+            <div className="air-cover-text2">Learn More</div>
         </>
     );
 };
