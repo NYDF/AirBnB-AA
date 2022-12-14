525bff5c8eafa73131d33f7b367e7cba73547195
nydf <yusu.us@gmail.com>
2022-12-14 11:53:58 -0500

    delete some warnings

--- frontend/src/components/Booking/CheckBooking/index.js
+++ frontend/src/components/Booking/CheckBooking/index.js
@@ -1,7 +1,6 @@
 
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory, useParams } from "react-router-dom";
 import { Calendar } from 'react-date-range';
 import { thunkLoadBookingsOfSpot } from "../../../store/bookingReducer";
 import { getDaysArray } from "../../../Util/functions";
