a2131f743c6a7bc85248acc064031a9e7f26027b
nydf <yusu.us@gmail.com>
2022-12-18 18:25:06 -0500

    map done

--- frontend/src/components/Maps/Maps.js
+++ frontend/src/components/Maps/Maps.js
@@ -1,16 +1,83 @@
 // frontend/src/components/Maps/Maps.js
-import React from 'react';
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import React, { useState } from 'react';
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '2560px',
+  width: '1680px',
   height: '1860px',
 };
 
 const center = {
-  lat: -3.745,
-  lng: -38.523
+  lat: 40.70977216636848,
+  lng: -73.96819584374789
 };
+
+const markers = [
+  {
+    id: 1,
+    name: "$559",
+    position: {
+      lat: 40.775956845776456,
+      lng: -73.98960213025359
+    }
+  },
+  {
+    id: 2,
+    name: "$836",
+    position: {
+      lat: 40.74955966427739,
+      lng: -74.02510224374731
+    }
+  },
+  {
+    id: 3,
+    name: "$888",
+    position: {
+      lat: 40.77633024472602,
+      lng: -73.93335058792488
+    }
+  },
+  {
+    id: 4,
+    name: "$559",
+    position: {
+      lat: 40.70977216636848,
+      lng: -73.96819584374789
+    }
+  },
+  {
+    id: 5,
+    name: "$939",
+    position: {
+      lat: 40.711134787673394,
+      lng: -73.99128024253406
+    }
+  },
+  {
+    id: 6,
+    name: "$888",
+    position: {
+      lat: 40.761314160529196,
+      lng: -74.04849675724024
+    }
+  },
+  {
+    id: 7,
+    name: "$633",
+    position: {
+      lat: 40.824106047769696,
+      lng: -73.97473994374613
+    }
+  },
+  {
+    id: 8,
+    name: "$369",
+    position: {
+      lat: 40.73895722480879,
+      lng: -73.98635305908984
+    }
+  }
+];
 
 
 const Maps = ({ apiKey }) => {
@@ -20,15 +87,38 @@ const Maps = ({ apiKey }) => {
     googleMapsApiKey: apiKey,
   });
 
+  const [activeMarker, setActiveMarker] = useState(null);
+
+  const handleActiveMarker = (marker) => {
+    if (marker === activeMarker) {
+      return;
+    }
+    setActiveMarker(marker);
+  };
+
+
   return (
     <>
       {isLoaded && (
         <GoogleMap
           mapContainerStyle={containerStyle}
           center={center}
-          zoom={10}
+          zoom={13}
         >
-          <Marker position={center}/>
+          {markers.map(({ id, name, position }) => (
+            <Marker
+              key={id}
+              position={position}
+              // label={name}
+              onClick={() => handleActiveMarker(id)}
+            >
+              {activeMarker === id ? (
+                <InfoWindow>
+                  <div>{name}</div>
+                </InfoWindow>
+              ) : null}
+            </Marker>
+          ))}
         </GoogleMap>
       )}
     </>
