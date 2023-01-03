641f8cb1953920b1b92efe092e154fbef83ae1c7
nydf <yusu.us@gmail.com>
2023-01-03 12:21:24 -0500

    aws in middle

--- frontend/src/components/Image/AddSpotImage/index.js
+++ frontend/src/components/Image/AddSpotImage/index.js
@@ -4,31 +4,63 @@ import { useParams } from 'react-router-dom';
 import { thunkAddSpotImg } from "../../../store/spotReducer";
 import './AddSpotImage.css'
 
+const validExtensions = [
+  'jpeg',
+  'jpg',
+  'png',
+  'svg'
+]
+
 function AddSpotImage() {
 
   const dispatch = useDispatch();
   const [url, setUrl] = useState('');
   const { spotId } = useParams();
+  const [spotFile, setSpotFile] = useState(null);
   const [hasSubmitted, setHasSubmitted] = useState("");
 
 
   const handleAddImg = async (e) => {
-    e.preventDefault();
-    setHasSubmitted(true);
+    e.preventDefault()
+    let errors = false;
+    if (spotFile.length > 0) {
+      const urlArr = spotFile.name.split('.');
+      const ext = urlArr[urlArr.length - 1];
+      if (!validExtensions.includes(ext.toLocaleLowerCase())) {
+        errors = true
+        alert(`Image format is invalid. Please upload Png, jpg, jpeg, svg format. `)
+        return
+      }
+    }
 
-    const imgPayload = { url, preview: false }
-    let addedImage = await dispatch(thunkAddSpotImg(imgPayload, spotId)).catch(async (res) => {
+    if (errors) return;
 
-      const data = await res.json();
-    });
+    const formData = new FormData()
+    formData.append("file", spotFile)
+    formData.append("preview", false)
+    dispatch(thunkAddSpotImg(formData, spotId))
+  }
+
+  const setFile = (e) => {
+    const file = e.target.files[0]
+    setSpotFile(file)
   }
 
   return (
     <div>
-      <h1 className='All-review'>add images</h1>
+      
+      <h1>add images</h1>
       <button
-        className="delete-spot-button"
+        className="edit-spot-add-image"
         onClick={handleAddImg}>Add Images</button>
+
+      <input
+        id='browse-files'
+        className='choose-image-input'
+        type='file'
+        accept="image/*"
+        onChange={setFile}
+      />
 
     </div>
   )
