098f10a2e2f7e10ecbd6c72a9126823036edc6a8
nydf <yusu.us@gmail.com>
2023-01-05 20:14:26 -0500

    map not hc

--- frontend/src/components/Maps/Maps.js
+++ frontend/src/components/Maps/Maps.js
@@ -1,6 +1,7 @@
 // frontend/src/components/Maps/Maps.js
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
+import { useSelector } from 'react-redux';
 import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
 
 const containerStyle = {
@@ -13,74 +14,6 @@ const center = {
   lng: -73.96819584374789
 };
 
-const markers = [
-  {
-    id: 1,
-    name: "$559",
-    position: {
-      lat: 40.775956845776456,
-      lng: -73.98960213025359
-    }
-  },
-  {
-    id: 2,
-    name: "$836",
-    position: {
-      lat: 40.74955966427739,
-      lng: -74.02510224374731
-    }
-  },
-  {
-    id: 3,
-    name: "$888",
-    position: {
-      lat: 40.77633024472602,
-      lng: -73.93335058792488
-    }
-  },
-  {
-    id: 4,
-    name: "$559",
-    position: {
-      lat: 40.70977216636848,
-      lng: -73.96819584374789
-    }
-  },
-  {
-    id: 5,
-    name: "$939",
-    position: {
-      lat: 40.711134787673394,
-      lng: -73.99128024253406
-    }
-  },
-  {
-    id: 6,
-    name: "$888",
-    position: {
-      lat: 40.761314160529196,
-      lng: -74.04849675724024
-    }
-  },
-  {
-    id: 7,
-    name: "$633",
-    position: {
-      lat: 40.824106047769696,
-      lng: -73.97473994374613
-    }
-  },
-  {
-    id: 8,
-    name: "$369",
-    position: {
-      lat: 40.73895722480879,
-      lng: -73.98635305908984
-    }
-  }
-];
-
-
 const Maps = ({ apiKey }) => {
   // console.log({apiKey})
   const history = useHistory();
@@ -89,13 +22,18 @@ const Maps = ({ apiKey }) => {
     googleMapsApiKey: apiKey,
   });
 
+  let spots = useSelector(state => state.spot)
+  let spotsArr = Object.values(spots)
+  // console.log(spotsArr)
+
+  let markers=[]
+  spotsArr.forEach(ele=>markers.push({id:ele.id, name:('$ '+ele.price).toString(),
+  position:{lat:ele.lat, lng:ele.lng}}))
+  // console.log('markers^^^^^^^^^^^^^^^^^^^^', markers)
+
   const [activeMarker, setActiveMarker] = useState(null);
 
   const handleActiveMarker = (marker) => {
-    // if (marker === activeMarker) {
-    //   return;
-    // }
-    // setActiveMarker(marker);
     history.push(`/spots/${marker}`)
   };
 
@@ -107,9 +45,7 @@ const Maps = ({ apiKey }) => {
           mapContainerStyle={containerStyle}
 
           // da3b2e5f9cdbd038
-          // id='da3b2e5f9cdbd038'
-          // mapIds='da3b2e5f9cdbd038'
-          // mapId='da3b2e5f9cdbd038'
+
           center={center}
           zoom={13}
         >
