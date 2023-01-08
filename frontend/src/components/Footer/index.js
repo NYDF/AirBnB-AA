53b5375baf417e04af8b1f46c09c26c845f7d547
nydf <yusu.us@gmail.com>
2023-01-08 18:56:38 -0500

    link to

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import linkedin from '../../img/linkedin.png'
 import github from '../../img/github.png'
+import portfolio from '../../img/portfolio.png'
+import angellist from '../../img/angellist.png'
 import './Footer.css';
 
 function Footer() {
@@ -12,6 +14,8 @@ function Footer() {
         <div className='About-link-port'>Link to Developer's</div>
         <a target="_blank" rel="noopener noreferrer" href=" https://www.linkedin.com/in/dongfang-song-25261218a/"><img id='linkedin' src={linkedin} /></a>
         <a target="_blank" rel="noopener noreferrer" href=" https://github.com/NYDF"><img id='github' src={github} /></a>
+        <a target="_blank" rel="noopener noreferrer" href=" https://angel.co/u/dongfang-song"><img id='angellist' src={angellist} /></a>
+        <a target="_blank" rel="noopener noreferrer" href=" https://nydf.github.io/"><img id='portfolio' src={portfolio} /></a>
       </div>
     </>
   );
