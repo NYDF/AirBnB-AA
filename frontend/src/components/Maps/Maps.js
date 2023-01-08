171c8cca44cd201b77c8e5e591d0b3921a16a41a
nydf <yusu.us@gmail.com>
2023-01-08 16:43:53 -0500

    test

--- frontend/src/components/Maps/Maps.js
+++ frontend/src/components/Maps/Maps.js
@@ -14,6 +14,73 @@ const center = {
   lng: -73.96819584374789
 };
 
+const markers = [
+  {
+    id: 1,
+    name: "$559",
+    position: {
+      lat: 40.775956845776456,
+      lng: -73.98960213025359
+    }
+  },
+  {
+    id: 2,
+    name: "$836",
+    position: {
+      lat: 40.74955966427739,
+      lng: -74.02510224374731
+    }
+  },
+  {
+    id: 3,
+    name: "$888",
+    position: {
+      lat: 40.77633024472602,
+      lng: -73.93335058792488
+    }
+  },
+  {
+    id: 4,
+    name: "$559",
+    position: {
+      lat: 40.70977216636848,
+      lng: -73.96819584374789
+    }
+  },
+  {
+    id: 5,
+    name: "$939",
+    position: {
+      lat: 40.711134787673394,
+      lng: -73.99128024253406
+    }
+  },
+  {
+    id: 6,
+    name: "$888",
+    position: {
+      lat: 40.761314160529196,
+      lng: -74.04849675724024
+    }
+  },
+  {
+    id: 7,
+    name: "$633",
+    position: {
+      lat: 40.824106047769696,
+      lng: -73.97473994374613
+    }
+  },
+  {
+    id: 8,
+    name: "$369",
+    position: {
+      lat: 40.73895722480879,
+      lng: -73.98635305908984
+    }
+  }
+];
+
 const Maps = ({ apiKey }) => {
   // console.log({apiKey})
   const history = useHistory();
@@ -26,9 +93,9 @@ const Maps = ({ apiKey }) => {
   let spotsArr = Object.values(spots)
   // console.log(spotsArr)
 
-  let markers=[]
-  spotsArr.forEach(ele=>markers.push({id:ele.id, name:('$ '+ele.price).toString(),
-  position:{lat:ele.lat, lng:ele.lng}}))
+  // let markers=[]
+  // spotsArr.forEach(ele=>markers.push({id:ele.id, name:('$ '+ele.price).toString(),
+  // position:{lat:ele.lat, lng:ele.lng}}))
   // console.log('markers^^^^^^^^^^^^^^^^^^^^', markers)
 
   const [activeMarker, setActiveMarker] = useState(null);
@@ -52,7 +119,6 @@ const Maps = ({ apiKey }) => {
           {markers.map(({ id, name, position }) => (
             <Marker
               key={id}
-              color='#FF385C'
               position={position}
               // label={name}
               title={name}
